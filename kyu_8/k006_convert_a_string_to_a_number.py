7921f099d66c838148117aa8d13ce6ea02714d66
aa <aa@MacBook-Pro-aa.local>
2026-04-30 16:29:57 +0300

    20260430 convert a string to a number

---
+++ kyu_8/k006_convert_a_string_to_a_number.py
@@ -0,0 +1,25 @@
+"""
+Convert a String to a Number!
+
+Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+Description
+We need a function that can transform a string into a number. What ways of achieving this do you know?
+Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral
+number.
+
+Examples
+"1234" --> 1234
+"605"  --> 605
+"1405" --> 1405
+"-7" --> -7
+"""
+
+
+def string_to_number(s: str) -> int:
+    return int(s)
+
+
+assert string_to_number("1234") == 1234
+assert string_to_number("605") == 605
+assert string_to_number("1405") == 1405
+assert string_to_number("-7") == -7
