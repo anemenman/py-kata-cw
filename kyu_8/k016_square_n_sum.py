f731bbe040e4e9ad2a5523e08614d005e8f39ab1
aa <aa@MacBook-Pro-aa.local>
2026-05-18 16:16:34 +0300

    20260518 square n sum

---
+++ kyu_8/k016_square_n_sum.py
@@ -0,0 +1,17 @@
+"""
+Square(n) Sum
+
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+For example, for [1, 2, 2] it should return 9 because 1^2+2^2+2^2=9
+"""
+
+
+def square_sum(numbers: list[int]) -> int:
+    return sum(x ** 2 for x in numbers)
+
+
+assert square_sum([1, 2]) == 5
+assert square_sum([0, 3, 4, 5]) == 50
+assert square_sum([]) == 0
+assert square_sum([-1, -2]) == 5
+assert square_sum([-1, 0, 1]) == 2
