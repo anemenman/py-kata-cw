0d958a4f07be028e81baa785f587e0c39eec9ed9
aa <aa@MacBook-Pro-aa.local>
2026-05-18 16:02:25 +0300

    20260518 reversed strings reversed()

--- kyu_8/k015_reversed_strings.py
+++ kyu_8/k015_reversed_strings.py
@@ -12,7 +12,16 @@ def solution(s: str) -> str:
     return s[::-1]  # slicing
 
 
+def solution2(s: str) -> str:
+    return ''.join(reversed(s))
+
+
 assert solution('world') == 'dlrow'
 assert solution('hello') == 'olleh'
 assert solution('') == ''
 assert solution('h') == 'h'
+
+assert solution2('world') == 'dlrow'
+assert solution2('hello') == 'olleh'
+assert solution2('') == ''
+assert solution2('h') == 'h'
