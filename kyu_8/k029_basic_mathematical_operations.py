fcc8e2c822b10b8998d9349e528287da7d8ecd6c
aa <aa@MacBook-Pro-aa.local>
2026-06-04 14:58:14 +0300

    20260604 basic mathematical operations tests v2

--- kyu_8/k029_basic_mathematical_operations.py
+++ kyu_8/k029_basic_mathematical_operations.py
@@ -42,3 +42,8 @@ assert basic_op('+', 4, 7) == 11
 assert basic_op('-', 15, 18) == -3
 assert basic_op('*', 5, 5) == 25
 assert basic_op('/', 49, 7) == 7
+
+assert basic_op_v2('+', 4, 7) == 11
+assert basic_op_v2('-', 15, 18) == -3
+assert basic_op_v2('*', 5, 5) == 25
+assert basic_op_v2('/', 49, 7) == 7
