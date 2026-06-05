c4bc93358543d7b2ba06153e4c5d2d91b5d6c47b
aa <aa@MacBook-Pro-aa.local>
2026-06-05 17:58:39 +0300

    20260605 grasshopper summation

---
+++ kyu_8/k030_grasshopper_summation.py
@@ -0,0 +1,23 @@
+"""
+Grasshopper - Summation
+
+Summation
+Write a program that finds the summation of every number from 1 to num (both inclusive). The number will always be a
+positive integer greater than 0. Your function only needs to return the result, what is shown between parentheses in
+the example below is how you reach that result and it's not part of it, see the sample tests.
+
+For example (Input -> Output):
+
+2 -> 3 (1 + 2)
+8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+"""
+
+
+def summation(num):
+    return num * (num + 1) // 2
+
+
+assert summation(1) == 1
+assert summation(8) == 36
+assert summation(22) == 253
+assert summation(100) == 5050
