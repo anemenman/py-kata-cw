9d79c5b81105b0782a8691c9d55b7948108ee5c9
aa <aa@MacBook-Pro-aa.local>
2026-06-09 18:49:54 +0300

    20260609 find the smallest integer in the array

---
+++ kyu_8/k032_find_the_smallest_integer_in_the_array.py
@@ -0,0 +1,20 @@
+"""
+Find the smallest integer in the array
+
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+"""
+
+
+def find_smallest_int(arr):
+    return min(arr)
+
+
+assert find_smallest_int([78, 56, 232, 12, 11, 43]) == 11
+assert find_smallest_int([78, 56, -2, 12, 8, -33]) == -33
+assert find_smallest_int([0, 1 - 2 ** 64, 2 ** 64]) == 1 - 2 ** 64
