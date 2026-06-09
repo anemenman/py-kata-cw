0de1210621e9675e29d4ea2ff888670ad4b5b49c
aa <aa@MacBook-Pro-aa.local>
2026-06-09 19:06:30 +0300

    20260609 return negative

---
+++ kyu_8/k033_return_negative.py
@@ -0,0 +1,23 @@
+"""
+Return Negative
+
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already
+negative?
+
+Examples
+make_negative(1);  # return -1
+make_negative(-5); # return -5
+make_negative(0);  # return 0
+Notes
+The number can be negative already, in which case no change is required.
+Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+"""
+
+
+def make_negative(number):
+    return -abs(number)
+
+
+assert make_negative(42) == -42
+assert make_negative(-9) == -9
+assert make_negative(0) == 0
