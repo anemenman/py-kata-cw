6239ead052e540b6a72334b83e5ab66621029032
aa <aa@MacBook-Pro-aa.local>
2026-06-10 17:01:09 +0300

    20260610 multiply

---
+++ kyu_8/k037_multiply.py
@@ -0,0 +1,17 @@
+"""
+Multiply
+
+This code does not execute properly. Try to figure out why.
+def multiply(a, b):
+    a * b
+"""
+
+
+def multiply(a, b):
+    return a * b
+
+
+assert multiply(2, 1) == 2
+assert multiply(5, 0) == 0
+assert multiply(0, 5) == 0
+assert multiply(0, 0) == 0
