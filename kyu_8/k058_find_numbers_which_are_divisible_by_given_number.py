22b9f5499d3a6c3e4a35d773624fc0dd27a58162
aa <aa@MacBook-Pro-aa.local>
2026-06-19 16:54:11 +0300

    20260619 find numbers which are divisible by given number

---
+++ kyu_8/k058_find_numbers_which_are_divisible_by_given_number.py
@@ -0,0 +1,19 @@
+"""
+Find numbers which are divisible by given number
+
+Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor.
+First argument is an array of numbers and the second is the divisor.
+
+Example(Input1, Input2 --> Output)
+[1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
+"""
+
+
+def divisible_by(numbers, divisor):
+    return [num for num in numbers if num % divisor == 0]
+
+
+assert divisible_by([1, 2, 3, 4, 5, 6], 2) == [2, 4, 6]
+assert divisible_by([1, 2, 3, 4, 5, 6], 3) == [3, 6]
+assert divisible_by([0, 1, 2, 3, 4, 5, 6], 4) == [0, 4]
+assert divisible_by([0], 4) == [0]
