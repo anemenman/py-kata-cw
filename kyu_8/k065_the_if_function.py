a79489ddf41c977ee45f4c3b60720907fd0f3462
aa <aa@MacBook-Pro-aa.local>
2026-07-02 20:11:58 +0300

    20260702 the if function tets

--- kyu_8/k065_the_if_function.py
+++ kyu_8/k065_the_if_function.py
@@ -21,3 +21,28 @@ def _if(value: Any, truthy_func: Callable[[], Any], falsy_func: Callable[[], Any
         return truthy_func()
     else:
         return falsy_func()
+
+
+for falsy in (False, None, 0, 0.0, '', [], (), {}, set(), range(0)):
+    def on_truthy():
+        return 'T'
+
+
+    def on_falsy():
+        return 'F'
+
+
+    result = _if(falsy, on_truthy, on_falsy)
+    assert result == 'F'
+
+for truly in (True, 1, -1, 0.1, ' ', '0', 'False', [0], (None,), {'a': 1}, object()):
+    def on_truthy():
+        return 'T'
+
+
+    def on_falsy():
+        return 'F'
+
+
+    result = _if(truly, on_truthy, on_falsy)
+    assert result == 'T'
