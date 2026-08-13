45e69d64be170e9f5220e185e3f1b3060fc209b5
aa <aa@MacBook-Pro-aa.local>
2026-08-13 15:56:22 +0300

    20260813 how good are you really

---
+++ kyu_8/k092_how_good_are_you_really.py
@@ -0,0 +1,40 @@
+"""
+How good are you really?
+
+There was a test in your class and you passed it. Congratulations!
+
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return true if you're better, else false!
+
+Note:
+Your points are not included in the array of your class's points. Do not forget them when calculating the average score!
+"""
+
+
+def better_than_average(class_points, your_points):
+    average = (sum(class_points) + your_points) / (len(class_points) + 1)
+    return your_points > average
+
+
+def better_than_average2(class_points, your_points):
+    return your_points * len(class_points) > sum(class_points)
+
+
+assert better_than_average([2, 3], 5) == True
+assert better_than_average([100, 40, 34, 57, 29, 72, 57, 88], 75) == True
+assert better_than_average([12, 23, 34, 45, 56, 67, 78, 89, 90], 69) == True
+assert better_than_average([41, 75, 72, 56, 80, 82, 81, 33], 50) == False
+assert better_than_average([29, 55, 74, 60, 11, 90, 67, 28], 21) == False
+assert better_than_average([100, 90, 80], 85) == False
+assert better_than_average([50, 50, 50], 50) == False
+
+assert better_than_average2([2, 3], 5) == True
+assert better_than_average2([100, 40, 34, 57, 29, 72, 57, 88], 75) == True
+assert better_than_average2([12, 23, 34, 45, 56, 67, 78, 89, 90], 69) == True
+assert better_than_average2([41, 75, 72, 56, 80, 82, 81, 33], 50) == False
+assert better_than_average2([29, 55, 74, 60, 11, 90, 67, 28], 21) == False
+assert better_than_average2([100, 90, 80], 85) == False
+assert better_than_average2([50, 50, 50], 50) == False
