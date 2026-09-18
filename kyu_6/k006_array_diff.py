b45807bb342d7484ead71dfe6d94e9b866cbd0d2
aa <aa@MacBook-Pro-aa.local>
2026-09-18 15:22:24 +0300

    20260918 array diff

---
+++ kyu_6/k006_array_diff.py
@@ -0,0 +1,26 @@
+"""
+Array.diff
+
+Implement a function that computes the difference between two lists. The function should remove all occurrences of
+elements from the first list (a) that are present in the second list (b). The order of elements in the first list
+should be preserved in the result.
+
+Examples
+If a = [1, 2] and b = [1], the result should be [2].
+
+If a = [1, 2, 2, 2, 3] and b = [2], the result should be [1, 3].
+"""
+
+
+def array_diff(a, b):
+    set_b = set(b)
+
+    return [item for item in a if item not in set_b]
+
+
+assert array_diff([1, 2], [1]) == [2]
+assert array_diff([1, 2, 2], [1]) == [2, 2]
+assert array_diff([1, 2, 2], [2]) == [1]
+assert array_diff([1, 2, 2], []) == [1, 2, 2]
+assert array_diff([], [1, 2]) == []
+assert array_diff([1, 2, 3], [1, 2]) == [3]
