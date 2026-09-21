02e382e2cc490ed351a0a9fde4ac3d7e30103bc5
aa <aa@MacBook-Pro-aa.local>
2026-09-21 12:47:44 +0300

    20260921 count characters in your string v2

--- kyu_6/k007_count_characters_in_your_string.py
+++ kyu_6/k007_count_characters_in_your_string.py
@@ -13,7 +13,22 @@ def count(text):
     return dict(Counter(text))
 
 
+from collections import defaultdict
+
+
+def count_v2(text):
+    result = defaultdict(int)
+    for char in text:
+        result[char] += 1
+    return dict(result)
+
+
 assert count('aba') == {'a': 2, 'b': 1}
 assert count('') == {}
 assert count('aa') == {'a': 2}
 assert count('aabb') == {'b': 2, 'a': 2}
+
+assert count_v2('aba') == {'a': 2, 'b': 1}
+assert count_v2('') == {}
+assert count_v2('aa') == {'a': 2}
+assert count_v2('aabb') == {'b': 2, 'a': 2}
